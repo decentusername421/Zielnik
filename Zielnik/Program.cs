29730b353e9e9bcd044810dc34f40c9713ac06f1
ola <olawalkowiak.xx@gmail.com>
2026-06-01 17:59:12 +0200

    Configure Identity services in Program.cs
    Added Identity services for user authentication.

--- Zielnik/Program.cs
+++ Zielnik/Program.cs
@@ -11,7 +11,12 @@ builder.Services.AddDbContext<ZielnikDbContext>(options =>
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services
+    .AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ZielnikDbContext>()
+    .AddDefaultTokenProviders();
 builder.Services.AddControllersWithViews();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
