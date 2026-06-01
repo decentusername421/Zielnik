b2ea4510f2735a5dbea74615b79d58be03e30bfa
ola <olawalkowiak.xx@gmail.com>
2026-06-01 18:05:58 +0200

    Implement database migration and seeding
    Add database migration and seeding on application startup

--- Zielnik/Program.cs
+++ Zielnik/Program.cs
@@ -23,6 +23,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ZielnikDbContext>();
+    context.Database.Migrate();
+    SeedData.Seed(context);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
