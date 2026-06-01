6b5d03f3f7108a0ff30b893449462f9e5eaf4f5b
ola <olawalkowiak.xx@gmail.com>
2026-06-01 19:56:24 +0200

    Refactor seed data population for plant categories

--- Zielnik/Data/SeedData.cs
+++ Zielnik/Data/SeedData.cs
@@ -8,15 +8,27 @@ namespace Zielnik.Data
         {
             if (context.PlantCategories.Any()) return;
 
-            var categories = new List<PlantCategory>
+            var categoryNames = new[]
             {
-                new PlantCategory { Name = "Warzywa" },
-                new PlantCategory { Name = "Owoce" },
-                new PlantCategory { Name = "Kwiaty" },
-                new PlantCategory { Name = "Drzewa" }
+                "Warzywa", "Owoce", "Kwiaty", "Drzewa",
+                "Pomidory", "Ogórki", "Papryki",
+                "Krzewy", "Zioła", "Ozdobne",
+                "Byliny", "Trawy", "Jednoroczne",
+                "Wieloletnie", "Dwuletnie",
+                "Cebulkowe", "Pnącza"
             };
 
-            context.PlantCategories.AddRange(categories);
+            foreach (var name in categoryNames)
+            {
+                if (!context.PlantCategories.Any(c => c.Name == name))
+                {
+                    context.PlantCategories.Add(new PlantCategory
+                    {
+                        Name = name
+                    });
+                }
+            }
+
             context.SaveChanges();
         }
     }
